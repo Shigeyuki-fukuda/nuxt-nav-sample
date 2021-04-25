75bde0a16415daa70d115671a532a0c2bdacf55c
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-04-25 23:03:49 +0900

    feat: Tailwind CSSのデフォルトのブレイクポイント設定を変更

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: { max: '767px' },
+      md: { min: '768px' },
+      lg: { min: '1024px' },
+      xl: { min: '1280px' },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
