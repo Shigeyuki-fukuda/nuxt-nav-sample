13d0c67d17b703f7ab549cdfffb486ea085555a5
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-04-25 22:51:50 +0900

    feat: ナビゲーションにTailwind CSSのユーティリティクラスを追加

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,25 +1,53 @@
 <template>
   <div>
-    <header>
-      <div>
-        <div>
+    <header class="w-full absolute md:static bg-black px-2 py-2 z-50">
+      <div class="md:max-w-3xl mx-auto md:flex md:items-center">
+        <div
+          class="w-full md:flex mx-auto px-6 md:px-0 flex justify-between items-center h-16"
+        >
           <h1>
-            <nuxt-link to="/">Nuxt Cafe</nuxt-link>
+            <nuxt-link
+              class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap"
+              to="/"
+            >
+              Nuxt Cafe
+            </nuxt-link>
           </h1>
-          <div>
-            <!-- TODO: ハンバーガーメニュー実装 -->
+          <div class="text-white md:hidden">
+            <button class="focus:outline-none">
+              <!-- ハンバーガーメーニューのSVG：https://reffect.co.jp/html/tailwind-for-beginners-navigation-menu -->
+              <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"
+                />
+              </svg>
+            </button>
           </div>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <nuxt-link to="/shop">店舗情報</nuxt-link>
+        <nav
+          class="w-full md:block absolute left-0 md:static bg-black md:bg-none z-20"
+        >
+          <ul class="md:flex md:justify-end md:items-end">
+            <li class="w-full md:w-auto md:ml-5">
+              <nuxt-link
+                to="/shop"
+                class="text-white sm:hover:bg-gray-600 md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                >店舗情報</nuxt-link
+              >
             </li>
-            <li>
-              <nuxt-link to="/menu">メニュー</nuxt-link>
+            <li class="w-full md:w-auto md:ml-5">
+              <nuxt-link
+                to="/menu"
+                class="text-white sm:hover:bg-gray-600 md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                >メニュー</nuxt-link
+              >
             </li>
-            <li>
-              <nuxt-link to="/information">お知らせ</nuxt-link>
+            <li class="w-full md:w-auto md:ml-5">
+              <nuxt-link
+                to="/information"
+                class="text-white sm:hover:bg-gray-600 md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                >お知らせ</nuxt-link
+              >
             </li>
           </ul>
         </nav>
@@ -38,5 +66,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style></style>
