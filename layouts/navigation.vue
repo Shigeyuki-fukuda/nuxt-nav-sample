d19f3e32733a9022c0f257be2efdeb8b99351619
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2022-05-07 14:44:57 +0900

    fix: ハンバーガーメニューをtabでフォーカス移動出来るように修正

--- layouts/navigation.vue
+++ layouts/navigation.vue
@@ -13,32 +13,31 @@
               Nuxt Cafe
             </nuxt-link>
           </h1>
-          <div class="text-white md:hidden">
-            <button class="focus:outline-none" @click="toggleStatus">
-              <!-- ハンバーガーメーニューのSVG：https://reffect.co.jp/html/tailwind-for-beginners-navigation-menu -->
-              <svg
-                v-show="!showMenu"
-                class="w-6 h-6 fill-current"
-                viewBox="0 0 24 24"
-              >
-                <title>メニューを開くボタン</title>
-                <path
-                  d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"
-                />
-              </svg>
-              <svg
-                v-show="showMenu"
-                id="gNav"
-                class="w-6 h-6 fill-current"
-                viewBox="0 0 24 24"
-              >
-                <title>メニューを閉じるボタン</title>
-                <path
-                  d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
-                />
-              </svg>
-            </button>
-          </div>
+          <button
+            v-show="!showMenu"
+            class="text-white md:hidden focus:outline-none"
+            @click="toggleStatus"
+          >
+            <!-- ハンバーガーメーニューのSVG：https://reffect.co.jp/html/tailwind-for-beginners-navigation-menu -->
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" tabindex="0">
+              <title>メニューを開くボタン</title>
+              <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+            </svg>
+          </button>
+          <button
+            v-show="showMenu"
+            tabindex="0"
+            class="text-white md:hidden focus:outline-none"
+            @click="toggleStatus"
+          >
+            <!-- ハンバーガーメーニューのSVG：https://reffect.co.jp/html/tailwind-for-beginners-navigation-menu -->
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" tabindex="0">
+              <title>メニューを閉じるボタン</title>
+              <path
+                d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+              />
+            </svg>
+          </button>
         </div>
         <nav
           :class="showMenu ? 'sm:block' : 'sm:hidden'"
